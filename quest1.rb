42f70e234966764ba889ade065cf973a1c8db3cb
MakerSquare Student <students@themakersquare.com>
2014-02-18 01:50:54 -0600

    added for 1000

--- quest1.rb
+++ quest1.rb
@@ -1,5 +1,5 @@
 array = []
-(1..100).each { |i| if i%5 == 0 || i%3 == 0  then array << i end}
+(1..1000).each { |i| if i%5 == 0 || i%3 == 0  then array << i end}
 
 print array.inject{|sum, x| x + sum }
 
